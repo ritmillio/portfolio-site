13ce27e83b29c0650fbcd4c00d18d4a35f25f30a
Zoltan Fodor <email@zoltanfodor.eu>
2022-07-18 14:56:58 +0200

    Animated courser

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import dynamic from 'next/dynamic'
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+  ssr: false
+});
 
 const Home: NextPage = () => {
   return (
     <div>
-      <div className='container mx-auto h-screen'>
-        <div className='flex items-center justify-center'>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={1.7}
+        outerAlpha={0}
+        hasBlendMode={true}
+        outerStyle={{
+          border: '3px solid var(--cursor-color)'
+        }}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+      />
+      <div className='container mx-auto'>
+        <div className='flex items-center justify-center h-screen'>
           Hola
         </div>
       </div>
