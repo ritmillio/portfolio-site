bd9d1e790eee24ced5948f03ab7b427202eca9bc
Zoltan Fodor <email@zoltanfodor.eu>
2022-07-18 15:28:10 +0200

    creating a Cursor component

---
+++ components/cursor.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+const AnimatedCursor: any = dynamic(() => import('react-animated-cursor'), {
+  ssr: false
+});
+
+const Cursor: React.FC = () => {
+  return <AnimatedCursor
+            innerSize={8}
+            outerSize={35}
+            innerScale={1}
+            outerScale={1.7}
+            outerAlpha={0}
+            hasBlendMode={true}
+            outerStyle={{
+            border: '3px solid var(--cursor-color)'
+            }}
+            innerStyle={{ 
+            backgroundColor: 'var(--cursor-color)'
+            }}
+        />
+}
+
+export default Cursor
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,10 @@
 import type { NextPage } from 'next'
-import dynamic from 'next/dynamic'
-const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
-  ssr: false
-});
+import Cursor from '../components/cursor'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={35}
-        innerScale={1}
-        outerScale={1.7}
-        outerAlpha={0}
-        hasBlendMode={true}
-        outerStyle={{
-          border: '3px solid var(--cursor-color)'
-        }}
-        innerStyle={{
-          backgroundColor: 'var(--cursor-color)'
-        }}
-      />
+      <Cursor />
       <div className='container mx-auto'>
         <div className='flex items-center justify-center h-screen'>
           Hola
