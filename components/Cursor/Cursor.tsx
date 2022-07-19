7663c32119c0d4548ba2a6c1532e15e34594c877
Zoltan Fodor <email@zoltanfodor.eu>
2022-07-19 22:38:08 +0200

    defining a new cursor

--- components/Cursor/Cursor.tsx
+++ components/Cursor/Cursor.tsx
@@ -8,8 +8,8 @@ const AnimatedCursor: any = dynamic(() => import('react-animated-cursor'), {
 const Cursor: React.FC = () => {
   return <AnimatedCursor
             innerSize={8}
-            outerSize={35}
-            innerScale={1}
+            outerSize={70}
+            innerScale={1.2}
             outerScale={1.7}
             outerAlpha={0}
             hasBlendMode={true}
