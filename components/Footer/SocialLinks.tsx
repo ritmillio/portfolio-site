cd1f974985857b9ca733ba323a7aa2ab501579ab
Zoltan Fodor <email@zoltanfodor.eu>
2022-07-19 23:40:08 +0200

    add noreferrer

--- components/Footer/SocialLinks.tsx
+++ components/Footer/SocialLinks.tsx
@@ -7,7 +7,7 @@ const SocialLinks: React.FC = () => {
         <div>
          <div className="h-36 sm:h-40 md:h-48 bg-gray-100">
             <div className="h-full flex items-center justify-center">
-              <a className="mx-2 sm:mx-4  lg:mx-6" href="#" target="_blank">
+              <a className="mx-2 sm:mx-4  lg:mx-6" href="#" target="_blank" rel="noreferrer">
                 <Image
                         loading="lazy"
                         src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png"
@@ -16,7 +16,7 @@ const SocialLinks: React.FC = () => {
                         alt="Follow me on YouTube"
                 />
               </a>
-              <a className="mx-2 sm:mx-4  lg:mx-6" href="https://www.linkedin.com/in/zoltan-fodor-007/" target="_blank">
+              <a className="mx-2 sm:mx-4  lg:mx-6" href="https://www.linkedin.com/in/zoltan-fodor-007/" target="_blank" rel="noreferrer">
                 <div className="hidden lg:block">
                     <Image
                             loading="lazy"
@@ -36,64 +36,54 @@ const SocialLinks: React.FC = () => {
                     />
                 </div>
               </a>
+              <a className="mx-2 sm:mx-4  lg:mx-6" href="https://github.com/ritmillio" target="_blank" rel="noreferrer">
+                <Image
+                        loading="lazy"
+                        src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+                        width={50}
+                        height={50}
+                        alt="Follow me on github and check out my work"
+                />
+              </a>
+              <a className="mx-2 sm:mx-4  lg:mx-6" href="https://medium.com/@zoltanfodor" target="_blank" rel="noreferrer">
+                <Image
+                        loading="lazy"
+                        src="https://cdn-icons-png.flaticon.com/512/5968/5968885.png"
+                        width={50}
+                        height={50}
+                        alt="Follow me on Medium"
+                />
+              </a>
+              <a className="mx-2 sm:mx-4  lg:mx-6" href="https://open.spotify.com/playlist/1DgMS1ijBNF6DXwFYEkqYf?si=3b0c4efa9524498f&pt=5438e1fb9ecb79520abae9fc20085d26" target="_blank" rel="noreferrer">
+                <Image
+                        loading="lazy"
+                        src="https://cdn.iconscout.com/icon/free/png-128/spotify-3771073-3147690.png"
+                        width={50}
+                        height={50}
+                        alt="Listen my dev music chanel on spotify"
+                />
+              </a>
 
-              {/* <a class="mx-2 sm:mx-4  lg:mx-6" data-cursor-hover href="" target="_blank">
-                <nuxt-img
-                          loading="lazy"
-                          src="https://cdn.iconscout.com/icon/free/png-128/twitter-241-721979.png"
-                          width="50"
-                          alt="Follow me on Twitter"
-                />
+              <a className="mx-2 sm:mx-4  lg:mx-6" href="https://www.buymeacoffee.com/zoltanfodor" target="_blank" rel="noreferrer">
+                <div className="hidden lg:block">
+                    <Image
+                            loading="lazy"
+                            src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-3.svg"
+                            width={165}
+                            height={100}
+                            alt="Support me via Buymeacoffee.com but just if you like my content"
+                    />
+                </div>
+                <div className="lg:hidden block">
+                    <Image
+                            loading="lazy"
+                            src="https://www.buymeacoffee.com/assets/img/guidelines/logo-mark-1.svg"
+                            width={50}
+                            height={50}
+                            alt="Support me via Buymeacoffee.com but just if you like my content"
+                    />
+                </div>
               </a>
-              
-              <a class="mx-2 sm:mx-4  lg:mx-6" data-cursor-hover href="https://github.com/ritmillio" target="_blank">
-                <nuxt-img
-                          loading="lazy"
-                          src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
-                          width="60"
-                          alt="My github profile"
-                />
-              </a>
-              <a class="mx-2 sm:mx-4  lg:mx-6" data-cursor-hover href="https://medium.com/@zoltanfodor" target="_blank">
-                <nuxt-img
-                          loading="lazy"
-                          src="https://cdn-icons-png.flaticon.com/512/5968/5968885.png"
-                          width="50"
-                          alt="Follow me on Medium so you can read my latest articles"
-                />
-              </a>
-              <a class="mx-2 sm:mx-4  lg:mx-6" data-cursor-hover href="https://discord.gg/CFDQDSRe" target="_blank">
-                <nuxt-img 
-                          loading="lazy"
-                          src="https://cdn.iconscout.com/icon/free/png-128/discord-3691244-3073764.png"
-                          width="50"
-                          alt="Join to my Discord channel."
-                />
-              </a>
-              <a class="mx-2 sm:mx-4  lg:mx-6" data-cursor-hover href="https://open.spotify.com/playlist/1DgMS1ijBNF6DXwFYEkqYf?si=3b0c4efa9524498f&pt=5438e1fb9ecb79520abae9fc20085d26" target="_blank">
-                <nuxt-img 
-                          loading="lazy"
-                          src="https://cdn.iconscout.com/icon/free/png-128/spotify-3771073-3147690.png"
-                          width="50"
-                          alt="Listen my dev music chanel on spotify"
-                />
-              </a>
-              <a class="mx-2 sm:mx-4  lg:mx-6" data-cursor-hover href="https://www.buymeacoffee.com/zoltanfodor" target="_blank">
-                <nuxt-img loading="lazy"
-                          class="hidden lg:block" 
-                          width="180"
-                          src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-3.svg" 
-                          alt="Support me via buymeacoffee.com"
-                />
-                <nuxt-img loading="lazy"
-                          width="50"
-                          height="25"
-                          class="rounded-xl lg:hidden"
-                          loding="lazy"
-                          src="https://www.buymeacoffee.com/assets/img/guidelines/logo-mark-1.svg"
-                          alt="Support me via buymeacoffee.com"
-                />
-              </a> */}
             </div>
           </div>
         </div>
