282794499fa6c22265b2924f1e9e1dd0ac391268
Zoltan Fodor <email@zoltanfodor.eu>
2022-07-20 15:31:10 +0200

    creating dark and light color palette based on happyhues.co/palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,39 @@ module.exports = {
   ],
   theme: {
     typography: (theme) => ({}),
-    extend: {},
+    extend: {
+      colors: {
+        // dark theme
+        // https://www.happyhues.co/palettes/4
+        drk: {
+          bgackground: '#16161a',
+          headline: '#fffffe',
+          paragh: '#94a1b2',
+          btn: '#7f5af0',
+          btntxt: '#fffffe',
+          stroke: '#010101',
+          main: '#fffffe',
+          highlight: '#7f5af0',
+          primary: '#2cb67d',
+          secondary: '#72757e'
+        },
+        // light theme
+        // https://www.happyhues.co/palettes/11
+        lght: {
+          bgackground: '#f9f4ef',
+          headline: '#020826',
+          paragh: '#716040',
+          btn: '#8c7851',
+          btntxt: '#fffffe',
+          stroke: '#020826',
+          main: '#fffffe',
+          highlight: '#8c7851',
+          primary: '#f25042',
+          secondary: '#eaddcf'
+        }
+
+      }
+    },
   },
   variants: {},
   plugins: [require('@tailwindcss/typography')],
