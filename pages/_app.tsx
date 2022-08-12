4b0c2d3e2551f821446c245ca6828b67abe2c463
Zoltan Fodor <me@zoltanfodor.dev>
2022-08-12 21:28:38 +0200

    Creating a basic dom loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,30 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Cursor from '../components/Cursor/Cursor'
 import { ThemeProvider } from 'next-themes'
+import { useState, useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(true), 6000);
+  }, []);
+
   return (
     <>
-      <ThemeProvider attribute="class">
+      {/* <ThemeProvider attribute="class">
         <Cursor />
         <Component {...pageProps} />
-      </ThemeProvider>
+      </ThemeProvider> */}
+
+      {loading ? (
+        <ThemeProvider attribute="class">
+          <Cursor />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      ) : (
+        <div className='h-screen bg-green-500 text-white flex items-center justify-center'>Loading</div>
+      )}
     </>
   )
 }
