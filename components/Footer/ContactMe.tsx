c456a586dcee3b8a0e42fdc10653f4fb29e33895
Zoltan Fodor <me@zoltanfodor.dev>
2022-08-30 18:10:06 +0200

    change default href mailto --> copy to clipboard

--- components/Footer/ContactMe.tsx
+++ components/Footer/ContactMe.tsx
@@ -1,7 +1,10 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Image from 'next/image'
 
 const ContactMe: React.FC = () => {
   const divRef = useRef<HTMLDivElement>(null)
+  const [value, isCopied] = useState(false);
 
   useEffect(() => {
     async function animate() {
@@ -27,7 +30,32 @@ const ContactMe: React.FC = () => {
               <span className='pl-6 sm:pl-8 md:pl-12 dark:text-pink-500 text-yellow-500 text-[48px] sm:text-[68px] md:text-[78px] lg:text-[88px]'>h</span><span className='dark:text-green-600 text-blue-300'>e</span><span className='dark:text-purple-700 text-pink-500 text-[42px] sm:text-[48px] md:text-[55px] lg:text-[64px]'>l</span><span className='dark:text-blue-400 text-green-500'>l</span><span className='dark:text-orange-600 text-orange-400'>o</span>
               &nbsp;?
             </h2>
-            <a href='mailto:me@zoltanfodor.dev' className='py-4 md:pt-6 text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold dark:hover:text-happyhues_4-background-button hover:text-red-700'>me@zoltanfodor.dev</a>
+
+            <CopyToClipboard onCopy={() => isCopied(true)} text="me@zoltanfodor.dev">
+              <div className='relative'>
+                <p className='py-4 md:pt-6 text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold dark:hover:text-happyhues_4-background-button hover:text-red-700'>me@zoltanfodor.dev
+                </p>
+                { value === true ?
+                  <div className='absolute top-0 left-0 -ml-16 sm:-ml-20 md:-ml-24 lg:-ml-28 mt-8 md:mt-10 lg:mt-12 flex flex-col items-center justify-center'>
+                    <Image 
+                      className='rotate-90'
+                      src="https://zoltanfodor.b-cdn.net/arrow-yellow.png"
+                      width={60}
+                      height={60}
+                      loading='lazy'
+                    />
+                    <span className='pt-2 text-3xl sm:text-4xl md:text-5xl lg:text-6xl uppercase dark:text-orange-500 font-semibold -rotate-[26deg]'>
+                      Copied
+                    </span>
+                  </div>
+            
+                  : null
+                }
+
+                {/* Add more padding */}
+                { value === true ? <div className='h-32 sm:h-28 md:h-24 lg:h-20'></div> : null }
+              </div>
+            </CopyToClipboard>
           </div>
         </>
       )
