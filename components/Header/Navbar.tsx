706f3c9f6ebb336bd44694234a7f0a32351cea35
Zoltan Fodor <me@zoltanfodor.dev>
2022-09-02 17:34:07 +0200

    finalize Nav bar

--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -1,18 +1,17 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import ProgressBar from './ProgressBar';
 import ThemeToggle from './ThemeToggle';
-import LocationAndWeather from './LocationAndWeather'
+import LocationAndWeather from './LocationAndWeather';
 
 const Navbar: React.FC = () => {
+  return (
+      <>
+        <ProgressBar />
+        <LocationAndWeather/>
+        <ThemeToggle />
+      </>
+    )
+}
   
-    return (
-        <>
-          <ProgressBar />
-          <LocationAndWeather/>
-          <ThemeToggle />
-        </>
-      )
-  }
-  
-  export default Navbar
+export default Navbar
   
--- components/Header/ProgressBar.tsx
+++ components/Header/ProgressBar.tsx
@@ -1,26 +1,25 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger, } from 'react-gsap';
 
 const ProgressBar: React.FC = () => {
 
-    const divRef = useRef(null);
-    useEffect(() => {
-      const el = divRef.current;
+  const divRef = useRef(null);
+  useEffect(() => {
+  const el = divRef.current;
+    gsap.to(el, {
+      value: 100,
+      ease: 'none',
+      scrollTrigger: { scrub: 0.3 }
+    });
+  }, [])
 
-      gsap.to(el, {
-        value: 100,
-        ease: 'none',
-        scrollTrigger: { scrub: 0.3 }
-      });
-    }, [])
-
-    return(
-        <>
-          <progress ref={divRef} max="100" value="0"></progress>
-          <ScrollTrigger></ScrollTrigger>
-        </>
-    )
+  return(
+      <>
+        <progress ref={divRef} max='100' value='0'></progress>
+        <ScrollTrigger></ScrollTrigger>
+      </>
+  )
 }
 
 export default ProgressBar;
