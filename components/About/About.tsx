e49869a282a9a8591c936656d0923ce746b011f4
Zoltan Fodor <me@zoltanfodor.dev>
2022-09-06 20:26:00 +0200

    Finalize about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react'
 import Image from 'next/image'
 import { reveal } from '../../reveal'
-import { logos } from '../About/about-logos'
+import AboutLogos from './AboutLogos'
  
 const About: React.FC = () => {
 
@@ -10,7 +10,7 @@ const About: React.FC = () => {
 
     useEffect(() => {
       async function animate() {
-          const sr = (await require("scrollreveal")).default(reveal)
+        const sr = (await require("scrollreveal")).default(reveal)
         if (headingRef.current) {
           sr.reveal(headingRef.current)
         }
@@ -26,31 +26,23 @@ const About: React.FC = () => {
     }, [])
 
     return (
-          <div className="h-screen flex flex-col items-center bg-[url('https://zoltanfodor.b-cdn.net/zoltan_fodor_portfolio_website/bg-noise-portfolio-website.png')] bg-center bg-repeat bg-[length:300px_300px] dark:bg-happyhues_4-background-secondary bg-happyhues_11-background-secondary">
+          <div className="min-h-screen flex flex-col items-center bg-[url('https://zoltanfodor.b-cdn.net/zoltan_fodor_portfolio_website/bg-noise-portfolio-website.png')] bg-center bg-repeat bg-[length:300px_300px] dark:bg-happyhues_4-background-secondary bg-happyhues_11-background-primary">
             <div id="about"></div>
-            <div className='py-20 px-4'>
+            <div className='py-20 px-2 sm:px-4'>
               <h2 className='w-fit uppercase text-5xl sm:text-6xl md:text-7xl lg:text-8xl dark:text-happyhues_4-text-headline text-happyhues_11-text-headline' ref= {headingRef}>About
               </h2>
               <h6 className='underline pt-2 pl-[1px] italic'>
                 Who am I ?
               </h6>
               <div className='flex flex-row-reverse items-center justify-center'>
-                <div className='flex flex-col ml-12'>
-                  <p className='text-white text-xl max-w-xl text-justify tracking-wider lg:mr-20 md:leading-relaxed lg:leading-relaxed'>
-                    I met Computer Science during a high school competition about building a company. We created a business idea/plan on the idea of a mobile application, the only problem was that nobody knew how to code so I started learning to code by myself. During my university years, I decided that I am going to pursue a career in the Frontend Development field since I loved and love using the browser as a canvas. <span className='font-bold font-mono italic'>Today I am focusing on helping companies to build a better UI experience, from small companies like a pizza shops to a big E-commerce giants such as Nespresso.</span> I am also interested in animating the web so I am constantly learning new things such as Locomotive Scroll, GSAP, and Three.js.
+                <div className='flex flex-col lg:ml-12 pt-4 lg:pt-0'>
+                  <p className='dark:text-happyhues_4-text-headline text-happyhues_11-text-headline text-lg md:text-xl max-w-xl text-left lg:text-justify tracking-wider lg:mr-20 md:leading-relaxed lg:leading-relaxed'>
+                  I was introduced to Computer Science during a high school competition when I had the opportunity to build a successful company from scratch. My team and I created a business plan based on the idea of a mobile application. The main difficulty that we faced as a team was the lack of knowledge about coding, which motivated me to learn how to code so I started to master it by myself to overcome that obstacle. During my studies at the university, I decided that I am going to pursue a career in the Frontend Development field since I always loved using the browser as a canvas. <span className='italic'>Currently, I am focusing on helping companies, from small companies such as local pizza shops to big E-commerce giants like Nespresso, to build a better UI experience.</span> I am also interested in animating the web, and I am constantly developing myself and learning new things such as Locomotive Scroll, GSAP, and Three.js.
                   </p>
-                  <p className='pt-4 text-2xl italic'>Some tools/libraries/frameworks/technologies I like to use</p>
-                  <div className='flex pt-2'>
-                    {logos.map(( logo: string ) => {
-                      return (
-                        <div className='mx-2'>
-                          <Image src={logo} width={32} height={32}/>
-                        </div>
-                      );
-                    })}
-                  </div>
+                  <p className='pt-4 text-lg md:text-xl lg:text-2xl italic'>Some tools/libraries/frameworks/technologies I like to use</p>
+                  <AboutLogos />
                 </div>
-                <div ref={imageRef} className='relative h-[400px] w-[250px] mr-auto hidden lg:block mt-12 ml-20'>
+                <div ref={imageRef} className='relative h-[400px] w-[250px] mr-auto hidden lg:block ml-20'>
                   <Image 
                       className='absolute rounded-md'
                       src='https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg'
--- components/About/about-logos.ts
+++
@@ -1,3 +0,0 @@
-export const logos = [
-    'https://zoltanfodor.b-cdn.net/icon-logos/html5-png.png', 'https://zoltanfodor.b-cdn.net/icon-logos/css3-png.png', 'https://zoltanfodor.b-cdn.net/icon-logos/tailwind-css-icon.png', 'https://zoltanfodor.b-cdn.net/icon-logos/javascript-png.png', 'https://zoltanfodor.b-cdn.net/icon-logos/typescript-png.png', 'https://zoltanfodor.b-cdn.net/icon-logos/git.png', 'https://zoltanfodor.b-cdn.net/icon-logos/github-png.png'
-]
