b727ea4d4c5d02c0c30eea336eefb7dcec828e8d
Zoltan Fodor <me@zoltanfodor.dev>
2022-09-07 10:05:18 +0200

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,25 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Cursor from '../components/Cursor/Cursor';
 import { ThemeProvider } from 'next-themes';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-NDZH17VXRN"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NDZH17VXRN');
+        `}
+      </Script>
       <ThemeProvider attribute="class">
           <Cursor />
           <Component {...pageProps} />
