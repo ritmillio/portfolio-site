9e3c899c1113ea85a2cf70961170a16e0bb74c33
Zoltan Fodor <me@zoltanfodor.dev>
2022-10-30 11:36:10 +0100

    remove spotify widget until creating a better solution

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Spotify from '../components/Spotify/Spotify'
+// import Spotify from '../components/Spotify/Spotify'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx:any) {
@@ -13,10 +13,11 @@ class MyDocument extends Document {
         <Head>
             <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe:ital@0;1&family=Oswald:wght@500;700&display=swap" rel="stylesheet" />
         </Head>
-        <body className='relative'>
-        <div className='fixed bottom-0 left-0 mb-20 ml-6 z-50'>
+        {/* <body className='relative'> */}
+        <body>
+        {/* <div className='fixed bottom-0 left-0 mb-20 ml-6 z-50'>
           <Spotify />
-        </div>
+        </div> */}
           <Main />
           <NextScript />
         </body>
