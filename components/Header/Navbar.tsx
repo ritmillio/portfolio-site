7f0049f6b9c164a2f601d8d5e063d6bad610b5d1
Zoltan Fodor <me@zoltanfodor.dev>
2023-11-07 22:37:14 +0100

    chore: comment out location and weather component

--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ProgressBar from './ProgressBar';
-import ThemeToggle from './ThemeToggle';
-import LocationAndWeather from './LocationAndWeather';
+import React from "react";
+import ProgressBar from "./ProgressBar";
+import ThemeToggle from "./ThemeToggle";
+import LocationAndWeather from "./LocationAndWeather";
 
 const Navbar: React.FC = () => {
   return (
-      <>
-        <ProgressBar />
-        <LocationAndWeather/>
-        <ThemeToggle />
-      </>
-    )
-}
-  
-export default Navbar
-  
+    <>
+      <ProgressBar />
+      {/* <LocationAndWeather/> */}
+      <ThemeToggle />
+    </>
+  );
+};
+
+export default Navbar;
