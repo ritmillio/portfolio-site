b35a001a15f6a459b5e9e1ce63b18faf6c1717af
Zoltan Fodor <me@zoltanfodor.dev>
2023-11-07 22:45:12 +0100

    chore: remove Custom Cursor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Cursor from '../components/Cursor/Cursor';
-import { ThemeProvider } from 'next-themes';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Cursor from "../components/Cursor/Cursor";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider attribute="class">
-          <Cursor />
-          <Component {...pageProps} />
+        {/* <Cursor /> */}
+        <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
