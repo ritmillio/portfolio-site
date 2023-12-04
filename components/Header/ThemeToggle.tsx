5f55897b14ce83a888603a8d3b65990367ccb3ad
Zoltan Fodor <me@zoltanfodor.dev>
2023-12-04 20:56:33 +0100

    feat: add lucid-icons to proj + fix theme toggle icons
    git status

--- components/Header/ThemeToggle.tsx
+++ components/Header/ThemeToggle.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import LucideIcon from "@/components/LucidIcon/Icon";
 
 const ProgressBar: React.FC = () => {
   const { theme, setTheme } = useTheme();
@@ -10,22 +11,18 @@ const ProgressBar: React.FC = () => {
   return (
     <div className="absolute top-0 right-0 mr-2 mt-2 sm:mr-4 sm:mt-4 md:mr-8 md:mt-8">
       {theme === "light" ? (
-        <Image
+        <LucideIcon
+          name="sun"
+          width={25}
+          height={25}
           onClick={() => setTheme("dark")}
-          src="https://zoltanfodor.b-cdn.net/sun-bright-rays-sunny-weather-33960.png"
-          alt="Light mode - Sun"
-          width={40}
-          height={40}
-          priority
         />
       ) : (
-        <Image
+        <LucideIcon
+          name="moon"
+          width={25}
+          height={25}
           onClick={() => setTheme("light")}
-          src="https://zoltanfodor.b-cdn.net/moon-2135874-1798952.png"
-          alt="Dark mode - Moon"
-          width={40}
-          height={40}
-          priority
         />
       )}
     </div>
