6ba6a8fe056dcde8c3bfdcf1a9b45c71db14a1c3
Zoltan Fodor <me@zoltanfodor.dev>
2023-12-04 21:21:30 +0100

    fix: udpate footer C section

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import ContactMe from './ContactMe'
-import SocialLinks from './SocialLinks'
+import React from "react";
+import ContactMe from "./ContactMe";
+import SocialLinks from "./SocialLinks";
 
 const Footer: React.FC = () => {
   return (
     <>
-      <div className='relative'>
+      <div className="relative">
         <div className="absolute top-0 -mt-8 h-10 w-full mx-auto"></div>
-        <div className='pt-12 dark:bg-[#101218] bg-happyhues_11-background-secondary'>
+        <div className="pt-12 dark:bg-[#101218] bg-happyhues_11-background-secondary">
           <ContactMe />
           <SocialLinks />
-          <div className="p-4">
-            <p id="copyright" className="text-center font-semibold dark:text-white text-black">
-              © 2022 Zoltan Fodor
+          <div className="flex flex-col items-center justify-center py-2">
+            <p
+              className="
+            text-neutral-200"
+            >
+              © Zoltan Fodor {new Date().getFullYear()}
             </p>
           </div>
         </div>
       </div>
     </>
-    )
-}
-  
-export default Footer
+  );
+};
+
+export default Footer;
