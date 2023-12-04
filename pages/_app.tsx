21cff6ca0daf5f41498768e5f5521b73c19ab21e
Zoltan Fodor <me@zoltanfodor.dev>
2023-12-04 21:17:02 +0100

    chore: update imports on pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Cursor from "../components/Cursor/Cursor";
+import Cursor from "@/components/Cursor/Cursor";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import type { NextPage } from 'next'
-import { NextSeo } from 'next-seo';
+import type { NextPage } from "next";
+import { NextSeo } from "next-seo";
 // Components ->
-import Navbar from '../components/Header/Navbar'
-import Intro from '../components/Intro/Intro'
-import About from '../components/About/About'
-import Projects from '../components/Projects/Projects'
-import Articles from '../components/Articles/Articles';
-import Footer from '../components/Footer/Footer'
+import Navbar from "@/components/Header/Navbar";
+import Intro from "@/components/Intro/Intro";
+import About from "@/components/About/About";
+import Projects from "@/components/Projects/Projects";
+import Articles from "@/components/Articles/Articles";
+import Footer from "@/components/Footer/Footer";
 
 const Home: NextPage = () => {
-
   return (
     <>
       <NextSeo
@@ -17,60 +16,65 @@ const Home: NextPage = () => {
         description="Hi , my name is Zoltan. I am a Creative Developer. I design and build websites and web applications. Let’s get in touch and work together on your next project."
         canonical="https://www.canonical.ie/"
         openGraph={{
-          url: 'https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg',
-          title: 'Zoltan Fodor - Creative Developer',
-          description: 'Hi , my name is Zoltan. I am a Creative Developer. I design and build websites and web applications. Let’s get in touch and work together on your next project',
+          url: "https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg",
+          title: "Zoltan Fodor - Creative Developer",
+          description:
+            "Hi , my name is Zoltan. I am a Creative Developer. I design and build websites and web applications. Let’s get in touch and work together on your next project",
           images: [
             {
-              url: 'https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg',
+              url: "https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg",
               width: 512,
               height: 768,
-              alt: 'Zoltan Fodor - Creative Developer',
-              type: 'image/jpeg',
+              alt: "Zoltan Fodor - Creative Developer",
+              type: "image/jpeg",
             },
             {
-              url: 'https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg',
+              url: "https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg",
               width: 612,
               height: 968,
-              alt: 'Zoltan Fodor - Creative Developer',
-              type: 'image/jpeg',
+              alt: "Zoltan Fodor - Creative Developer",
+              type: "image/jpeg",
             },
-            { url: 'https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg' },
-            { url: 'https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg' },
+            {
+              url: "https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg",
+            },
+            {
+              url: "https://zoltanfodor.b-cdn.net/zoltan-fodor-resi-prof.jpeg",
+            },
           ],
-          site_name: 'SiteName',
+          site_name: "SiteName",
         }}
         twitter={{
-          handle: '@handle',
-          site: '@site',
-          cardType: 'summary_large_image',
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
         }}
         additionalLinkTags={[
           {
-            rel: 'icon',
-            href: 'https://zoltanfodor.b-cdn.net/fav-zoli.png',
+            rel: "icon",
+            href: "https://zoltanfodor.b-cdn.net/fav-zoli.png",
           },
           {
-            rel: 'apple-touch-icon',
-            href: 'https://zoltanfodor.b-cdn.net/fav-zoli.png',
-            sizes: '76x76'
+            rel: "apple-touch-icon",
+            href: "https://zoltanfodor.b-cdn.net/fav-zoli.png",
+            sizes: "76x76",
           },
         ]}
-        />
-        <nav>
-          <Navbar />
-        </nav>
-        <Intro />
-        <main className='main'>
-          <About />
-          <Projects />
-          <Articles />
-        </main>
-        <footer>
-          <Footer />
-        </footer>
+      />
+      <nav>
+        <Navbar />
+      </nav>
+      <Intro />
+      <main className="main">
+        <About />
+        <Projects />
+        <Articles />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
